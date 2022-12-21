db3da2ca565fab2de8a3ab4dc99f471cf03e2a9f
Lsagar1996 <lakshmisagar000@gmail.com>
2022-12-21 15:13:34 +0530

    commit

--- src/test/java/com/HRM/Orange_module/OpenApplication.java
+++ src/test/java/com/HRM/Orange_module/OpenApplication.java
@@ -7,6 +7,8 @@ public class OpenApplication {
 	@Test(groups = "sanity")
 	public void test() {
 		System.out.println("sagar");
+		String BROWSER = System.getProperty("browser");
+		System.out.println(BROWSER);
 	}
 
 }
